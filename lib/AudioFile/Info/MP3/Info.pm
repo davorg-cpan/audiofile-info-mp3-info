8a890da1f3d6b91c130aa73956d04dc0c901f1e6
E. Choroba <choroba@matfyz.cz>
2024-01-02 12:02:42 +0100

    Fix a typo (MP -> MP3)

--- lib/AudioFile/Info/MP3/Info.pm
+++ lib/AudioFile/Info/MP3/Info.pm
@@ -5,7 +5,7 @@ AudioFile::Info::MP3::Info - Perl extension to get info from MP3 files.
 =head1 DESCRIPTION
 
 This is a plugin for AudioFile::Info which uses MP3::Info to get
-data about MP files.
+data about MP3 files.
 
 See L<AudioFile::Info> for more details.
 
